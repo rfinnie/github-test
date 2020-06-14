d83db263f9f05690bf3cff40cd92ba63a1ce891d
Ryan Finnie <ryan@finnie.org>
2020-06-14 14:54:13 -0700

    Test

--- test_sockets.py
+++ test_sockets.py
@@ -1,22 +1,24 @@
 import socket
+import sys
 
 
-addrinfo = socket.getaddrinfo(
-                    '127.0.0.1',
-                    50000,
-                    socket.AF_UNSPEC,
-                    socket.SOCK_DGRAM,
-                    socket.IPPROTO_UDP,
-)[0]
-sock_server = socket.socket(addrinfo[0], addrinfo[1], addrinfo[2])
-sock_server.bind(addrinfo[4])
+for port in range(49152, 65536):
+    addrinfo = socket.getaddrinfo(
+        "127.0.0.1", port, socket.AF_UNSPEC, socket.SOCK_DGRAM, socket.IPPROTO_UDP
+    )[0]
+    sock_server = socket.socket(addrinfo[0], addrinfo[1], addrinfo[2])
+    try:
+        sock_server.bind(addrinfo[4])
+    except socket.error as e:
+        print("Port {}: {}".format(port, e))
+
+sys.exit(0)
 sock_client = socket.socket(addrinfo[0], addrinfo[1], addrinfo[2])
 
 try:
     for i in range(1000000):
         sock_client.sendto(b"\x00", addrinfo[4])
         sock_server.recvfrom(16384)
-        #raise ValueError()
 except Exception:
     print(i)
     raise
